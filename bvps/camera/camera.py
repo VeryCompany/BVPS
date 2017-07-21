f71b35136e22b47767d0f4cdda33ef40a7ef10ff
李宏 <limingyang.chn@gmail.com>
2017-07-21 15:25:48 +0800

    fx

--- bvps/camera/camera.py
+++ bvps/camera/camera.py
@@ -198,7 +198,8 @@ class CameraCaptureThread(threading.Thread):
                         #latency.update(clock() - t0)
                     if len(pending) > 0 and num % 20 == 0:
                         log.debug("摄像头{}{}当前排队线程数{}个,线程池共{}个线程".format(self.cameraName,"拍摄中" if video.isOpened() else "已关闭",len(pending),threadn))
-                    if len(pending) < threadn and ret=video.grub():
+                    if len(pending) < threadn and video.grub():
+                        ret=true
                         if num % 20 == 0:
                             log.debug("ready to video read().....")
                         frame = cv2.RetrieveFrame(video)
