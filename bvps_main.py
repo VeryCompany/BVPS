daf874eed6cef3b71c342072482813ef19f1d0ce
李宏 <limingyang.chn@gmail.com>
2017-07-21 14:12:10 +0800

    fx

--- bvps_main.py
+++ bvps_main.py
@@ -28,28 +28,29 @@ try:
             "password": "",
             "type": CameraCmdType.START_CAPTURE,
             "port": 10000
-        },
-        "camera2": {
-            "x": "",
-            "y": "",
-            "z": "",
-            "device": "rtsp://192.168.0.205:554",
-            "user": "",
-            "password": "",
-            "type": CameraCmdType.START_CAPTURE,
-            "port": 10001
         }
         # ,
-        # "camera3": {
+        # "camera2": {
         #     "x": "",
         #     "y": "",
         #     "z": "",
-        #     "device": "rtsp://192.168.0.114:554",
+        #     "device": "rtsp://192.168.0.205:554",
         #     "user": "",
         #     "password": "",
         #     "type": CameraCmdType.START_CAPTURE,
-        #     "port": 10002
+        #     "port": 10001
         # }
+        ,
+        "camera3": {
+            "x": "",
+            "y": "",
+            "z": "",
+            "device": "rtsp://192.168.0.114:554",
+            "user": "",
+            "password": "",
+            "type": CameraCmdType.START_CAPTURE,
+            "port": 10002
+        }
     }
     #启动采集摄像头
     #todo:消息反馈处理和异常处理
