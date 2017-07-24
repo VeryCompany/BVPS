eed2caaead7febb3d684eb8ea166f6e33bea8bae
李宏 <limingyang.chn@gmail.com>
2017-07-24 13:38:50 +0800

    fx

--- bvps/camera/camera.py
+++ bvps/camera/camera.py
@@ -174,8 +174,8 @@ class CameraCaptureThread(threading.Thread):
                     #给用户的脸部中心画一个圈
                     cv2.circle(frame, (faceX,faceY), 5,  (0,255,0), thickness=3, lineType=8, shift=0)
                 #识别出用户，将用户的Id，图像坐标位置发送给中枢Actor做处理
-                if not self.camera.frameQueue.full():
-                   self.camera.frameQueue.put_nowait(frame)
+            if not self.camera.frameQueue.full():
+               self.camera.frameQueue.put_nowait(frame)
 
         except Exception, e:
             log.info(e.message)
