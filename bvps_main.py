7fb17d77b17fd0b3b821c392f02eb66c0ba94758
李宏 <limingyang.chn@gmail.com>
2017-07-24 17:07:26 +0800

    added all netcam

--- bvps_main.py
+++ bvps_main.py
@@ -78,6 +78,34 @@ try:
             "height":720,
             "fourcc":('M', 'J', 'P', 'G')
         }
+        ,
+        "camera4": {
+            "x": "",
+            "y": "",
+            "z": "",
+            "device": "rtsp://192.168.0.205:554",
+            "user": "",
+            "password": "",
+            "type": CameraCmdType.START_CAPTURE,
+            "port": 10004,
+            "frequency":30,
+            "width":1280,
+            "height":720
+        }
+        ,
+        "camera4": {
+            "x": "",
+            "y": "",
+            "z": "",
+            "device": "rtsp://192.168.0.114:554",
+            "user": "",
+            "password": "",
+            "type": CameraCmdType.START_CAPTURE,
+            "port": 10004,
+            "frequency":30,
+            "width":1280,
+            "height":720
+        }
     }
     #启动采集摄像头
     #todo:消息反馈处理和异常处理
