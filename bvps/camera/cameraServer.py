748c6fe3ee178b629c6491e609b0e29faada6907
李宏 <limingyang.chn@gmail.com>
2017-07-29 02:52:46 +0800

    fx

--- bvps/camera/cameraServer.py
+++ bvps/camera/cameraServer.py
@@ -28,7 +28,7 @@ import copy
 
 class TrainingServer(multiprocessing.Process):
     human_map = {
-        "unknown":[[[1,1,1,1,1,1,1,1],[123,131231,12,3,123,1,23,1]]]]
+        "unknown":[[[1,1,1,1,1,1,1,1],[123,131231,12,3,123,1,23,1]]]
     }
 
     def __init__(self, in_queue, out_queue):
