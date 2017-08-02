ea8ceaace011c7d2d697d30a9287c8e0b4002842
李宏 <limingyang.chn@gmail.com>
2017-08-02 15:04:26 +0800

    fx

--- bvps/camera/detector.py
+++ bvps/camera/detector.py
@@ -39,8 +39,8 @@ class DetectorProcessor(multiprocessing.Process):
             todo://比较画面是否有变化，如果没有变化可以不进行处理，提高效率！
             """
             frame, t0, secs = DetectorProcessor.frame_in.get()
-            #if count_times % 3 == 0:
-            #    continue
+            if DetectorProcessor.frame_in.qsize() > 20:
+                continue
             humans = self.detect_humans(frame, t0, secs)
             if len(humans) > 0:
                 log.debug("检测到{}个人".format(len(humans)))
