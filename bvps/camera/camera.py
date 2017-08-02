ff1bcb44d69d08ffd18b2f422c9f20160fe2f0af
李宏 <limingyang.chn@gmail.com>
2017-08-02 13:48:57 +0800

    fx

--- bvps/camera/camera.py
+++ bvps/camera/camera.py
@@ -175,7 +175,7 @@ class Camera(ActorTypeDispatcher):
                                         self.recognizer_in_q.qsize())
             if count_times > 10:
                 log.info(
-                    "{}预处理队列平均堆积{},识别器队列平均堆积{},训练器平均评价堆积{},识别器队列平均堆积{}".format(
+                    "{}预处理堆积{:0.1f},识别器堆积{:0.1f},训练器堆积{:0.1f},识别器堆积{:0.1f}".format(
                         self.cameraId, pre_queue_stat.value / count_times,
                         human_detector_q_stat.value / count_times,
                         training_dset_q_stat.value / count_times,
