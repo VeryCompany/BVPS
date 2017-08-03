08618ca9dddc82285a85d60d520101b5a6123031
李宏 <limingyang.chn@gmail.com>
2017-08-03 12:12:29 +0800

    fx

--- bvps/camera/pre_processor.py
+++ bvps/camera/pre_processor.py
@@ -4,6 +4,8 @@ import logging as log
 import multiprocessing
 from bvps.camera.common import clock, StatValue
 import cv2
+from multiprocessing.pool import ThreadPool
+from collections import deque
 
 
 class PreProcessor(multiprocessing.Process):
@@ -15,18 +17,31 @@ class PreProcessor(multiprocessing.Process):
         self.frame_interval = StatValue()
         self.last_frame_time = clock()
         self.latency = StatValue()
+
     def run(self):
         from bvps.config import cameras as ca
         scale = ca[self.camera.cameraId]["scale"]
+        threadn = cv2.getNumberOfCPUs()
+        pool = ThreadPool(processes=threadn * 2)
+        pending = deque()
         while True:
-            frame, t0, ts = PreProcessor.frame_in.get()
-            h, w, d = frame.shape
+            while len(pending) > 0 and pending[0].ready():
+                frame, t0, ts = pending.popleft().get()
+                PreProcessor.frame_out.put((frame, t0, ts))
+            if len(pending) < threadn:
+                frame, t0, ts = PreProcessor.frame_in.get()
+                task = pool.apply_async(self.resize_frame, (frame, scale, t0,
+                                                            ts))
+                pending.append(task)
+                t = clock()
+                self.latency.update(t - t0)
+                self.frame_interval.update(t - self.last_frame_time)
+                self.last_frame_time = t
+            log.debug("{},latency:{:0.1f}ms,process time:{:0.1f}ms".format(
+                self.camera.cameraId, self.latency.value * 1000,
+                self.frame_interval.value * 1000))
 
-            if scale is not None and scale != 1:
-                frame = cv2.resize(frame, (int(w * scale), int(h * scale)))
-            PreProcessor.frame_out.put((frame, t0, ts))
-            log.debug("{},latency:{:0.1f}ms,process time:{:0.1f}ms".format(self.camera.cameraId,self.latency.value * 1000, self.frame_interval.value*1000))
-            t = clock()
-            self.latency.update(t-t0)
-            self.frame_interval.update(t-self.last_frame_time)
-            self.last_frame_time = t
+    def resize_frame(self, frame, scale, t0, ts):
+        h, w, d = frame.shape
+        f = cv2.resize(frame, (int(w * scale), int(h * scale)))
+        return (f, t0, ts)
