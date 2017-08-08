5c3e8145df127e5c5d2c5122d319207e8edd1dda
李宏 <limingyang.chn@gmail.com>
2017-08-08 14:13:29 +0800

    fx

--- bvps/torch/torch_neural_net.py
+++ bvps/torch/torch_neural_net.py
@@ -68,6 +68,7 @@ class TorchNeuralNet:
             cutorch = lua.require('cutorch')
             lua.require('cunn')
             self._net = self._net._cuda()
+            self.net.setDevice(2)
             self._cuda_tensor = torch.CudaTensor(1, 3, imgDim, imgDim)
         self._cuda = cuda
         self._imgDim = imgDim
