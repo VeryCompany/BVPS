777f67ac8382851f40222c494cb3f4fe3018090e
李宏 <limingyang.chn@gmail.com>
2017-08-08 11:52:06 +0800

    fx

--- bvps/system/sysActor.py
+++ bvps/system/sysActor.py
@@ -1,23 +1,5 @@
 from thespian.actors import *
 import logging
-import SocketServer
-
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        #print "{} wrote:".format(self.client_address[0])
-        #print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
 
 class SystemActor(ActorTypeDispatcher):
     def __init__(self, *args, **kw):
