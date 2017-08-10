6bbec64b6ce1fb76df0dbb2fce6d3a8ad7aee1ec
李宏 <limingyang.chn@gmail.com>
2017-08-10 17:57:37 +0800

    fx

--- bvps_main.py
+++ bvps_main.py
@@ -36,9 +36,8 @@ try:
         cameras[camId]["address"] = cama
         log.info("启动摄像头{}，命令CameraCmdType.START_CAPTURE,address:{}".format(
             camId, cama))
-        asys.tell(cama, "start camera!!!!")
-        asys.tell(cama, "start camera!!!!")
-        asys.tell(cama, "start camera!!!!")
+        asys.tell(cama, "start camera:{}!!!!".format(camId))
+        asys.tell(cama, "start camera:{}!!!!".format(camId))
         #asys.tell(cama, CameraCmd(CameraCmdType.START_CAPTURE, camId, params))
 
 except KeyboardInterrupt:
