ec82fd6e286945665bf0be6e1b718c7447c24ed7
李宏 <limingyang.chn@gmail.com>
2017-08-14 14:57:41 +0800

    fx

--- bvps/camera/recognizer.py
+++ bvps/camera/recognizer.py
@@ -15,9 +15,9 @@ fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, '..', 'models')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 
-net = TorchNeuralNet(
-    os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), imgDim=96, cuda=True)
-
+#net = TorchNeuralNet(
+    #os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), imgDim=96, cuda=True)
+net = None
 
 class SVMRecognizer(multiprocessing.Process):
     def __init__(self, camera, in_queue, out_queue):
