75161e6b8a1b3497bafd9dc05236b3058720a2fb
李宏 <limingyang.chn@gmail.com>
2017-08-14 21:12:45 +0800

    fx

--- bvps/config.py
+++ bvps/config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from bvps.common import CameraType
-
 """摄像头配置信息"""
 cameras = {
     "camera1": {
@@ -32,67 +31,66 @@ cameras = {
             3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
             4: 720  # cv2.CAP_PROP_FRAME_HEIGHT
         }
+    },
+    "camera2": {
+        "groupId": "group_1",
+        "parameters": {
+            "x": "",
+            "y": "",
+            "z": "",
+            "angle_x": 0,
+            "angle_y": 0,
+            "angle_z": 0,
+            "focallength": 6,
+            "sensor_width": 4.8,
+            "sensor_height": 2.7,
+            "pixel_mm": 0.00375,
+            "distortion_ceof": {},
+            "camera_matrix": []
+        },
+        "scale": 1.0 / 4.0,
+        "device": "rtsp://192.168.0.82:554",
+        "user": "",
+        "password": "",
+        "cameraType": CameraType.POSITION,
+        "port": 10005,
+        "fourcc": ('M', 'J', 'P', 'G'),
+        "processNum": 4,
+        "video_properties": {
+            5: 25,  # cv2.CAP_PROP_FPS
+            3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
+            4: 720  # cv2.CAP_PROP_FRAME_HEIGHT
+        }
+    },
+    "camera3": {
+        "parameters": {
+            "x": "",
+            "y": "",
+            "z": "",
+            "angle_x": 0,
+            "angle_y": 0,
+            "angle_z": 0,
+            "focallength": 6,
+            "sensor_width": 4.8,
+            "sensor_height": 2.7,
+            "pixel_mm": 0.00375,
+            "distortion_ceof": {},
+            "camera_matrix": []
+        },
+        "scale": 1.0 / 4.0,
+        "device": "rtsp://192.168.0.80:554",
+        "user": "",
+        "password": "",
+        "cameraType": CameraType.CAPTURE,
+        "port": 10006,
+        "fourcc": ('M', 'J', 'P', 'G'),
+        "processNum": 4,
+        "video_properties": {
+            5: 25,  # cv2.CAP_PROP_FPS
+            3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
+            4: 720  # cv2.CAP_PROP_FRAME_HEIGHT
+        }
     }
-    # ,
-    # "camera2": {
-    #     "groupId": "group_1",
-    #     "parameters": {
-    #         "x": "",
-    #         "y": "",
-    #         "z": "",
-    #         "angle_x": 0,
-    #         "angle_y": 0,
-    #         "angle_z": 0,
-    #         "focallength": 6,
-    #         "sensor_width": 4.8,
-    #         "sensor_height": 2.7,
-    #         "pixel_mm": 0.00375,
-    #         "distortion_ceof": {},
-    #         "camera_matrix": []
-    #     },
-    #     "scale": 1.0/4.0,
-    #     "device": "rtsp://192.168.0.82:554",
-    #     "user": "",
-    #     "password": "",
-    #     "cameraType": CameraType.POSITION,
-    #     "port": 10005,
-    #     "fourcc": ('M', 'J', 'P', 'G'),
-    #     "processNum": 4,
-    #     "video_properties": {
-    #         5: 25,  # cv2.CAP_PROP_FPS
-    #         3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
-    #         4: 720  # cv2.CAP_PROP_FRAME_HEIGHT
-    #     }
-    # },
-    # "camera3": {
-    #     "parameters": {
-    #         "x": "",
-    #         "y": "",
-    #         "z": "",
-    #         "angle_x": 0,
-    #         "angle_y": 0,
-    #         "angle_z": 0,
-    #         "focallength": 6,
-    #         "sensor_width": 4.8,
-    #         "sensor_height": 2.7,
-    #         "pixel_mm": 0.00375,
-    #         "distortion_ceof": {},
-    #         "camera_matrix": []
-    #     },
-    #     "scale": 1.0/4.0,
-    #     "device": "rtsp://192.168.0.80:554",
-    #     "user": "",
-    #     "password": "",
-    #     "cameraType": CameraType.CAPTURE,
-    #     "port": 10006,
-    #     "fourcc": ('M', 'J', 'P', 'G'),
-    #     "processNum": 4,
-    #     "video_properties": {
-    #         5: 25,  # cv2.CAP_PROP_FPS
-    #         3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
-    #         4: 720  # cv2.CAP_PROP_FRAME_HEIGHT
-    #     }
-    # }
 }
 """双目摄像头分组信息"""
 camera_group = {
@@ -103,5 +101,4 @@ camera_group = {
         "cx": 0,
         "cy": 0
     }
-
 }
