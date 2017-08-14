574605613092d82667a48346aa3b4cc0180979de
李宏 <limingyang.chn@gmail.com>
2017-08-14 17:31:56 +0800

    fx

--- bvps/camera/trainer.py
+++ bvps/camera/trainer.py
@@ -27,9 +27,9 @@ fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, '..', 'models')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 
-net = TorchNeuralNet(
-    os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), imgDim=96, cuda=True)
-
+# net = TorchNeuralNet(
+#     os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), imgDim=96, cuda=True)
+net = None
 spg = [{
     'C': [1, 10, 100, 1000],
     'kernel': ['linear']
