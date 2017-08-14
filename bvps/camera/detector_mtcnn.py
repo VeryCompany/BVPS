51e3907e551e7010ed2e79525979b34c3f50515f
李宏 <limingyang.chn@gmail.com>
2017-08-15 00:47:53 +0800

    fx

--- bvps/camera/detector_mtcnn.py
+++ bvps/camera/detector_mtcnn.py
@@ -87,7 +87,7 @@ class DetectorProcessor(multiprocessing.Process):
             ],
             epoch=[16, 16, 16],
             batch_size=[2048, 256, 16],
-            ctx=mx.gpu(0),
+            ctx=[mx.gpu(0), mx.gpu(1)],
             thresh=[0.5, 0.5, 0.7],
             min_face_size=40,
             stride=2)
