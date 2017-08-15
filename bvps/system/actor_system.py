0947d220b4c93d5726fc5f02e880301a2c8f7360
李宏 <limingyang.chn@gmail.com>
2017-08-15 19:21:48 +0800

    fx

--- bvps/system/actor_system.py
+++ bvps/system/actor_system.py
@@ -2,6 +2,6 @@ from thespian.actors import ActorSystem
 from bvps.logger import logcfg
 
 print("-"*100)
-actor_system = ActorSystem(systemBase="multiprocQueueBase", logDefs=logcfg)
+actor_system = ActorSystem(systemBase="multiprocTCPBase", logDefs=logcfg)
 print("init actor system:{}".format(actor_system))
 print("-"*100)
