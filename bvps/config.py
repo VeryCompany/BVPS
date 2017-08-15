4602700d3bfdb530e55482bd7d8fac09b8c22715
李宏 <limingyang.chn@gmail.com>
2017-08-15 22:25:11 +0800

    fx

--- bvps/config.py
+++ bvps/config.py
@@ -28,7 +28,7 @@ cameras = {
         "processNum": 4,
         "DProcessNum": 3,
         "RProcessNum": 1,
-        "gpuId": 1,
+        "gpuId": 0,
         "video_properties": {
             5: 25,  # cv2.CAP_PROP_FPS
             3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
