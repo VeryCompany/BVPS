2e16b04743208fab9fd3fe81b00f55a58c98d129
李宏 <limingyang.chn@gmail.com>
2017-08-15 19:55:06 +0800

    fx

--- bvps/camera/recognizer.py
+++ bvps/camera/recognizer.py
@@ -44,13 +44,19 @@ class SVMRecognizer(multiprocessing.Process):
         return identity
 
     def run(self):
-        fileDir = os.path.dirname(os.path.realpath(__file__))
-        modelDir = os.path.join(fileDir, '..', 'models')
-        openfaceModelDir = os.path.join(modelDir, 'openface')
-        self.net = TorchNeuralNet(
-            os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'),
-            imgDim=96,
-            cuda=True)
+        try:
+            fileDir = os.path.dirname(os.path.realpath(__file__))
+            modelDir = os.path.join(fileDir, '..', 'models')
+            openfaceModelDir = os.path.join(modelDir, 'openface')
+            self.net = TorchNeuralNet(
+                os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'),
+                imgDim=96,
+                cuda=True)
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            log.error(
+                traceback.format_exception(exc_type, exc_value,
+                                           exc_traceback))
         while True:
             try:
                 msg = SVMRecognizer.in_queue.get()
