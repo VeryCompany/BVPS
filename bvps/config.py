7aa752cc381e25bf2b2df6bf07c70cc15d53e64f
李宏 <limingyang.chn@gmail.com>
2017-08-15 16:33:54 +0800

    fx

--- bvps/config.py
+++ bvps/config.py
@@ -26,7 +26,7 @@ cameras = {
         "port": 10004,
         "fourcc": ('M', 'J', 'P', 'G'),
         "processNum": 4,
-        "DProcessNum": 3,
+        "DProcessNum": 2,
         "RProcessNum": 1,
         "gpuId": 1,
         "video_properties": {
@@ -59,7 +59,7 @@ cameras = {
         "port": 10005,
         "fourcc": ('M', 'J', 'P', 'G'),
         "processNum": 4,
-        "DProcessNum": 3,
+        "DProcessNum": 2,
         "RProcessNum": 1,
         "gpuId": 1,
         "video_properties": {
@@ -91,9 +91,9 @@ cameras = {
         "port": 10006,
         "fourcc": ('M', 'J', 'P', 'G'),
         "processNum": 4,
-        "DProcessNum": 3,
+        "DProcessNum": 2,
         "RProcessNum": 1,
-        "gpuId": 0,
+        "gpuId": 1,
         "video_properties": {
             5: 25,  # cv2.CAP_PROP_FPS
             3: 1280,  # cv2.CAP_PROP_FRAME_WIDTH
