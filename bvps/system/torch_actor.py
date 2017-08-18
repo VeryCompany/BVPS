d15c3ed6f8e5ff13d9228afc25f84ca3c4be2360
李宏 <limingyang.chn@gmail.com>
2017-08-18 13:43:48 +0800

    fx

--- bvps/system/torch_actor.py
+++ bvps/system/torch_actor.py
@@ -5,13 +5,22 @@ from bvps.torch.torch_neural_net_lutorpy import TorchNeuralNet
 from thespian.troupe import troupe
 from thespian.actors import requireCapability
 
-# @troupe(max_count=10, idle_count=10)
-
-
-@requireCapability('torch')
+@troupe(max_count=10, idle_count=10)
 class TorchActor(ActorTypeDispatcher):
     def __init__(self, *args, **kw):
         log.info("ready to init torch actor.....")
+
+
+    def receiveMsg_tuple(self, message, sender):
+        cameraId, human, t0, sec, center, size = message
+        px, py = center
+        log.info("received identity request from {}, image.shape:{}".format(
+            cameraId, human.shape))
+        rep = self.net.forward(human)
+        self.send(sender, (cameraId, rep[0], center, t0, sec))
+
+    def _init_torch(self):
+        log.info("ready to lunch torch neura net....")
         fileDir = os.path.dirname(os.path.realpath(__file__))
         modelDir = os.path.join(fileDir, '..', 'models')
         openfaceModelDir = os.path.join(modelDir, 'openface')
@@ -19,11 +28,4 @@ class TorchActor(ActorTypeDispatcher):
             os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'),
             imgDim=96,
             cuda=True)
-
-    def receiveMsg_tuple(self, message, sender):
-        cameraId, human, t0, sec, center, size = message
-        px, py = center
-        rep = self.net.forward(human)
-        self.send(sender, (cameraId, rep[0], center, t0, sec))
-        log.info("received identity request from {}, image.shape:{}".format(
-            cameraId, human.shape))
+        log.info("lunch torch neura net ok ....")
