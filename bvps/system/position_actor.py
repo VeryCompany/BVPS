ff0bd957832ae8fe67e5bb525a045002317d8cf9
李宏 <limingyang.chn@gmail.com>
2017-08-22 20:53:42 +0800

    fx

--- bvps/system/position_actor.py
+++ bvps/system/position_actor.py
@@ -135,57 +135,61 @@ class PositionActor(ActorTypeDispatcher):
         camere_position = {}
         # for sec, group in sec_group:
         multi_position_xy = {}
-        for groupId, values in sec_group.items():
-            if len(values) < 2:
-                continue
-            camera_group_xy = {"groupId": groupId}
-            for camId,infos in values.items():
-                x_y = list(map(lambda info: [info[0], info[1]], infos))
-                # """需要考虑更好的滤波方式"""
-                sum_xy = reduce(lambda axy1, axy2: np.add(axy1, axy2), x_y)
-                # """当前时间1秒以内的像素平均坐标"""
-                average_xy = np.divide(sum_xy, len(x_y))
-                camera_group_xy[camId]["xy"] = average_xy
-                # pixel_mm = cms[camId]["parameters"]["pixel_mm"] / cms[camId]["scale"]
-                # focallength = cms[camId]["parameters"][focallength]
-                # camera_group_xy[camId]["pixel_mm"] = pixel_mm
-                # camera_group_xy[camId]["focallength"] = focallength
-                # if camId == cg["primary"]:
-                #     camera_group_xy["primary"] = camId
-                # else:
-                #     camera_group_xy["slave"] = camId
+        try:
+            for groupId, values in sec_group.items():
+                if len(values) < 2:
+                    continue
+                camera_group_xy = {"groupId": groupId}
+                for camId,infos in values.items():
+                    x_y = list(map(lambda info: [info[0], info[1]], infos))
+                    # """需要考虑更好的滤波方式"""
+                    sum_xy = reduce(lambda axy1, axy2: np.add(axy1, axy2), x_y)
+                    # """当前时间1秒以内的像素平均坐标"""
+                    average_xy = np.divide(sum_xy, len(x_y))
+                    camera_group_xy[camId]["xy"] = average_xy
+                    # pixel_mm = cms[camId]["parameters"]["pixel_mm"] / cms[camId]["scale"]
+                    # focallength = cms[camId]["parameters"][focallength]
+                    # camera_group_xy[camId]["pixel_mm"] = pixel_mm
+                    # camera_group_xy[camId]["focallength"] = focallength
+                    # if camId == cg["primary"]:
+                    #     camera_group_xy["primary"] = camId
+                    # else:
+                    #     camera_group_xy["slave"] = camId
 
-            multi_position_xy[groupId] = camera_group_xy
-            """单位时间的画面坐标"""
+                multi_position_xy[groupId] = camera_group_xy
+                """单位时间的画面坐标"""
 
-        for gp in multi_position_xy.items():
-            # groupId = multi_position_xy[gp]#gp["groupId"]
-            camera_group_xy = multi_position_xy[gp]
-            primary = None
-            slave = None
-            for camId in cg[gp]["members"]:
-                if camId == cg[gp]["primary"]:
-                    primary = camId
-                else:
-                    slave = camId
-            primary_xy = camera_group_xy[primary]["xy"]
-            slave_xy = camera_group_xy[slave]["xy"]
+            for gp in multi_position_xy.items():
+                # groupId = multi_position_xy[gp]#gp["groupId"]
+                camera_group_xy = multi_position_xy[gp]
+                primary = None
+                slave = None
+                for camId in cg[gp]["members"]:
+                    if camId == cg[gp]["primary"]:
+                        primary = camId
+                    else:
+                        slave = camId
+                primary_xy = camera_group_xy[primary]["xy"]
+                slave_xy = camera_group_xy[slave]["xy"]
 
-            x1 = primary_xy[0]
-            y1 = primary_xy[1]
-            x2 = slave_xy[0]
-            y2 = slave_xy[1]
+                x1 = primary_xy[0]
+                y1 = primary_xy[1]
+                x2 = slave_xy[0]
+                y2 = slave_xy[1]
 
-            baseline = int(cg[gp]["baseline_mm"])
-            d = abs(x1 - x2)
+                baseline = int(cg[gp]["baseline_mm"])
+                d = abs(x1 - x2)
 
-            f = int(cms[gp]["parameters"]["focallength"])
-            Z = f * baseline / d
+                f = int(cms[gp]["parameters"]["focallength"])
+                Z = f * baseline / d
 
-            X = x1 * Z / f
-            Y = y1 * Z / f
+                X = x1 * Z / f
+                Y = y1 * Z / f
 
-            # camere_position = {}
-            camere_position[gp] = [X, Y, Z]
-            log.info("position X->:{} Y->:{} Z->:{}".format(X, Y, Z))
-        return camere_position
+                # camere_position = {}
+                camere_position[gp] = [X, Y, Z]
+                log.info("position X->:{} Y->:{} Z->:{}".format(X, Y, Z))
+        except Exception as e:
+            log.error(e)
+        finally:
+            return camere_position
